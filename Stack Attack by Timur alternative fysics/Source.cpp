9457c4d0979affea0c1e1ed5e290b32d85a66f14
БингоТак БингоТак <bingotaak@outlook.com>
2022-10-25 20:53:33 +0400

    dffghj

--- Stack Attack by Timur alternative fysics/Source.cpp
+++ Stack Attack by Timur alternative fysics/Source.cpp
@@ -3,10 +3,12 @@
 #include <thread>
 #include <malloc.h>
 #include <windows.h>
-#include <conio.h>
+//#include <conio.h>
 #include <ctime>
+//#include <Mmsystem.h>
 
-#define HEIGH_ROOM 10
+
+#define HEIGH_ROOM 30
 #define WIDTH_ROOM 12
 
 int ROOM[WIDTH_ROOM][HEIGH_ROOM];
@@ -16,8 +18,7 @@ int playerPosX = rand() % WIDTH_ROOM;
 int playerPosY = 0;
 long long JumpTime;
 int score = 0;
-int NewBlockPosX;
-int NewBlockPosY;
+int GameSpeed = 1000;
 
 bool MayMoveBlock(int room[WIDTH_ROOM][HEIGH_ROOM], int x1, int x2, int y) {
 	if ((room[x2][y] == 0) && (room[x1][y + 1] == 0) && (x2 >= 0) && (x2 < WIDTH_ROOM)) {
@@ -31,87 +32,106 @@ bool IfDropping(int room[WIDTH_ROOM][HEIGH_ROOM], int x, int y) {
 	}
 	else return(false);
 }
-void DebagWriting() {
-	while (1 && !exiting) {
-		std::cout << "SCORE: " << score << "\n";
-		for (int j = HEIGH_ROOM - 1; j >= 0; j--) {
-			for (int i = 0; i < WIDTH_ROOM; i++) {
-				std::cout << ROOM[i][j] << "     ";
-			}
-			std::cout << "\n";
-		}
-		std::cout << "\n" << "\n" << "\n";
-		Sleep(1000);
-	}
-}
 void MoveBlock(int room[WIDTH_ROOM][HEIGH_ROOM], int x1, int y1, int x2, int y2) {
 	room[x2][y2] = room[x1][y1];
 	room[x1][y1] = 0;
 }
 void SpawnNewBlocks() {
 	while (1 && !exiting) {
+		int NewBlockPosX;
+		int NewBlockPosY;
 		NewBlockPosX = rand() % WIDTH_ROOM;
 		NewBlockPosY = HEIGH_ROOM - 1;
 		ROOM[NewBlockPosX][NewBlockPosY] = rand() % 3 + 1;
-		while (IfDropping(ROOM, NewBlockPosX, NewBlockPosY)) {
-			MoveBlock(ROOM, NewBlockPosX, NewBlockPosY, NewBlockPosX, NewBlockPosY - 1);
-			NewBlockPosY--;
-			Sleep(1000);
-			if (ROOM[NewBlockPosX][NewBlockPosY - 1] == 9) {
-				exiting = true;
-				std::cout << "\n\n\n\n\n";
-				std::cout << "GAME OVER" << "\n";
-				std::cout << "Your score: " << score;
-				break;
-			}
-		}
+		Sleep(GameSpeed*5);
 	}
 }
-void ButtonCheck() {
-	while (1) {
-		char ch;
-		int code;
-		ch = _getche();
-		code = static_cast<int>(ch);
-		if (ch == 27) {// если клавиша esc
-			exiting = true;
-			std::cout << "EXITING";
-			break;
-		}
-		if ((ch == 65) || (ch == 97)) { // если клавиша A
-			//std::cout << "A";
-			if ((ROOM[playerPosX - 1][playerPosY] == 0) && (ROOM[playerPosX - 1][playerPosY + 1] == 0) && (playerPosX - 1 >= 0)) {
-				//std::cout << "A";
-				MoveBlock(ROOM, playerPosX, playerPosY, playerPosX - 1, playerPosY);
-				MoveBlock(ROOM, playerPosX, playerPosY + 1, playerPosX - 1, playerPosY + 1);
-				playerPosX--;
+void DebagWriting() {
+	while (1 && !exiting) {
+		std::cout << "SCORE: " << score << " ";
+		if (GameSpeed == 1000)
+			std::cout << "Game Speed: x1";
+		else if (GameSpeed == 500)
+			std::cout << "Game Speed: x2";
+		else if (GameSpeed == 250)
+			std::cout << "Game Speed: x4";
+		else
+			std::cout << "Game Speed: x8";
+		std::cout << "\n";
+		for (int j = 1; j < HEIGH_ROOM; j++) {
+			for (int i = 0; i < WIDTH_ROOM; i++) {
+				if (ROOM[i][j] != 9) {
+					if (ROOM[i][j] != 0) {
+						if (ROOM[i][j - 1] == 9) {
+							exiting = true;
+							std::cout << "\a\n\n\n\n\n";
+							std::cout << "GAME OVER" << "\n";
+							std::cout << "Your score: " << score << "\n";
+							break;
+						}
+						if (IfDropping(ROOM, i, j))
+							if (ROOM[i][j] != 9) {
+								MoveBlock(ROOM, i, j, i, j - 1);
+							}
+					}
+				}
 			}
 		}
-		if ((ch == 68) || (ch == 100)) { // если клавиша D
-			//std::cout << "D";
-			if ((ROOM[playerPosX + 1][playerPosY] == 0) && (ROOM[playerPosX + 1][playerPosY + 1] == 0) && (playerPosX + 1 < WIDTH_ROOM)) {
-				//std::cout << "D";
-				MoveBlock(ROOM, playerPosX, playerPosY, playerPosX + 1, playerPosY);
-				MoveBlock(ROOM, playerPosX, playerPosY + 1, playerPosX + 1, playerPosY + 1);
-				playerPosX++;
+		for (int j = HEIGH_ROOM - 1; j >= 0; j--) {
+			for (int i = 0; i < WIDTH_ROOM; i++) {
+				if (ROOM[i][j] == 0) {
+					std::cout << "| |";
+				}
+				else if (ROOM[i][j] == 1) {
+					std::cout << "(/)";
+				}
+				else if (ROOM[i][j] == 2) {
+					std::cout << "(@)";
+				}
+				else if (ROOM[i][j] == 3) {
+					std::cout << "(#)";
+				}
+				else if (ROOM[i][j] == 9) {
+					std::cout << "-|-";
+				}
+				else {
+					std::cout << ROOM[i][j];
+				}
+				std::cout << "     ";
 			}
+			std::cout << "\n";
 		}
-		if (ch == 32) {
-			std::cout << "SPACE";
-			if (clock() / CLK_TCK - JumpTime >= 3) {
-				std::cout << "jump";
-				MoveBlock(ROOM, playerPosX, playerPosY + 1, playerPosX, playerPosY + 2);
-				MoveBlock(ROOM, playerPosX, playerPosY, playerPosX, playerPosY + 1);
-				playerPosY++;
-				JumpTime = clock() / CLK_TCK;
-			}
-		}
+		std::cout << "\n" << "\n" << "\n";
+		Sleep(GameSpeed);
 	}
+}
+void DroppingBlocks() {
+	while (1 && !exiting) {
+		for (int j = 1; j < HEIGH_ROOM; j++) {
+			for (int i = 0; i < WIDTH_ROOM; i++) {
+				if (ROOM[i][j] != 9) {
+					if (ROOM[i][j] != 0) {
+						if (ROOM[i][j - 1] == 9) {
+							exiting = true;
+							std::cout << "\a\n\n\n\n\n";
+							std::cout << "GAME OVER" << "\n";
+							std::cout << "Your score: " << score;
+							break;
+						}
+						if (IfDropping(ROOM, i, j))
+							if (ROOM[i][j] != 9) {
+								MoveBlock(ROOM, i, j, i, j - 1);
+							}
+					}
+				}
+			}
+		}
+		Sleep(GameSpeed);
+	}	
 }
 
 
 int main() {
-
 	for (int j = 0; j < HEIGH_ROOM; j++) {
 		for (int i = 0; i < WIDTH_ROOM; i++) {
 			ROOM[i][j] = 0;
@@ -126,19 +146,34 @@ int main() {
 	//#ifdef DEBUG
 	std::thread DEBUGth(DebagWriting);
 	//#endif
-	//std::thread Buttons(ButtonCheck);
+	//std::thread BlocksDrop(DroppingBlocks);
+	time_t timeTAB = 0;
 	time_t timeESC = 0;
 	time_t timeA = 0;
 	time_t timeD = 0;
 	time_t timeSPACE = 0;
 	while (1) { // main cycle
-		if ((GetKeyState(27) & 0x8000) && (clock() / CLK_TCK - timeESC >= 1)) {// если клавиша esc
+		if ((GetKeyState(27) & 0x8000) && (clock() * 1. / CLK_TCK - timeESC >= 1.5)) {// если клавиша esc
 			timeESC = clock() / CLK_TCK;
 			exiting = true;
-			std::cout << "EXITING";
+			std::cout << "\aEXITING...";
 			break;
 		}
-		if ((GetKeyState('A') & 0x8000) && (clock() / CLK_TCK - timeA >= 1)) { // если клавиша A
+		if ((GetKeyState(9) & 0x8000) && (clock() * 1. / CLK_TCK - timeTAB >= 1.5)) {// если клавиша TAB
+			timeTAB = clock() / CLK_TCK;
+			//MessageBeep(MB_OK);
+			PlaySound(TEXT("D:\Sound_55962300 1662746220 (online-audio-converter.com).wav"), 0, SND_ASYNC);
+			//std::cout << "TAB";
+			if (GameSpeed == 1000)
+				GameSpeed = 500;
+			else if(GameSpeed == 500)
+				GameSpeed = 250;
+			else if (GameSpeed == 250)
+				GameSpeed = 125;
+			else
+				GameSpeed = 1000;
+		}
+		if ((GetKeyState('A') & 0x8000) && (clock()*1. / CLK_TCK - timeA >= 1.5)) { // если клавиша A
 			timeA = clock() / CLK_TCK;
 			//std::cout << "A";
 			if ((ROOM[playerPosX - 1][playerPosY] == 0) && (ROOM[playerPosX - 1][playerPosY + 1] == 0) && (playerPosX - 1 >= 0)) {
@@ -148,21 +183,15 @@ int main() {
 				playerPosX--;
 			}
 			else if ((MayMoveBlock(ROOM, playerPosX - 1, playerPosX - 2, playerPosY)) && (clock() / CLK_TCK - JumpTime >= 3)) {
-				std::cout << "LeftPush";
+				//std::cout << "LeftPush";
 				MoveBlock(ROOM, playerPosX - 1, playerPosY, playerPosX - 2, playerPosY);
-				int BlockPosY = playerPosY;
-				while (IfDropping(ROOM, playerPosX - 2, BlockPosY)) {
-					//std::cout << "drop";
-					MoveBlock(ROOM, playerPosX - 2, BlockPosY, playerPosX - 2, BlockPosY - 1);
-					BlockPosY--;
-				}
 
 				MoveBlock(ROOM, playerPosX, playerPosY, playerPosX - 1, playerPosY);
 				MoveBlock(ROOM, playerPosX, playerPosY + 1, playerPosX - 1, playerPosY + 1);
 				playerPosX--;
 			}
 		}
-		if ((GetKeyState('D') & 0x8000) && (clock() / CLK_TCK - timeD >= 1)) { // если клавиша D
+		if ((GetKeyState('D') & 0x8000) && (clock() * 1. / CLK_TCK - timeD >= 1.5)) { // если клавиша D
 			timeD = clock() / CLK_TCK;
 			//std::cout << "D";
 			if ((ROOM[playerPosX + 1][playerPosY] == 0) && (ROOM[playerPosX + 1][playerPosY + 1] == 0) && (playerPosX + 1 < WIDTH_ROOM)) {
@@ -172,21 +201,15 @@ int main() {
 				playerPosX++;
 			}
 			else if ((MayMoveBlock(ROOM, playerPosX + 1, playerPosX + 2, playerPosY)) && (clock() / CLK_TCK - JumpTime >= 3)) {
-				std::cout << "RightPush";
+				//std::cout << "RightPush";
 				MoveBlock(ROOM, playerPosX + 1, playerPosY, playerPosX + 2, playerPosY);
-				int BlockPosY = playerPosY;
-				while (IfDropping(ROOM, playerPosX + 2, BlockPosY)) {
-					//std::cout << "drop";
-					MoveBlock(ROOM, playerPosX + 2, BlockPosY, playerPosX + 2, BlockPosY - 1);
-					BlockPosY--;
-				}
 
 				MoveBlock(ROOM, playerPosX, playerPosY, playerPosX + 1, playerPosY);
 				MoveBlock(ROOM, playerPosX, playerPosY + 1, playerPosX + 1, playerPosY + 1);
 				playerPosX++;
 			}
 		}
-		if ((GetKeyState(32) & 0x8000) && (clock() / CLK_TCK - timeSPACE >= 1)) {
+		if ((GetKeyState(32) & 0x8000) && (clock() * 1. / CLK_TCK - timeSPACE >= 1.5)) {
 			timeSPACE = clock() / CLK_TCK;
 			//std::cout << "SPACE";
 			if (clock() / CLK_TCK - JumpTime >= 4) {
@@ -214,27 +237,15 @@ int main() {
 				}
 			}
 			if (!ifZero) {
+				MessageBeep(MB_OK);
 				score++;
-				for (int i = 1; i < HEIGH_ROOM; i++) {
-					for (int j = 0; j < WIDTH_ROOM; j++) {
-						if (((i != NewBlockPosY) && (i != NewBlockPosY + 1)) || (j != NewBlockPosX))
-							MoveBlock(ROOM, j, i, j, i - 1);
-					}
+				for (int i = 0; i < WIDTH_ROOM; i++) {
+					ROOM[i][0] = 0;
 				}
-				playerPosY--;
 			}
 		}
-		/*
-		if (ROOM[playerPosX][playerPosY + 2] != 0) {
-			exiting = true;
-			std::cout << "\n\n\n\n\n";
-			std::cout << "GAME OVER" << "\n";
-			std::cout << "Your score: " << score;
-			break;
-		}*/
 	}
-
-	//Buttons.join();
+	//BlocksDrop.join();
 	SpawnBlocks.join();
 	DEBUGth.join();
 }
